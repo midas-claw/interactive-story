c395da65894f2fb784a74d5659daa628df822b3b
midas_claw <edragon.rocker@gmail.com>
2015-09-27 12:50:55 -0400

    Send name from main to story activity

--- app/src/main/java/com/smartdragons/interactivestory/MainActivity.java
+++ app/src/main/java/com/smartdragons/interactivestory/MainActivity.java
@@ -25,13 +25,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = mNameField.getText().toString();
-                startStory();
+                startStory(name);
             }
         });
     }
 
-    private void startStory() {
+    private void startStory(String name) {
         Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
         startActivity(intent);
     }
 }
