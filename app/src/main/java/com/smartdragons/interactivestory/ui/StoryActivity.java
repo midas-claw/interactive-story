9493cab95df54651b2b6d1e74e2b8b2d0e1bcdb6
midas_claw <edragon.rocker@gmail.com>
2015-09-27 23:39:51 -0400

    Remove harcoded page values

--- app/src/main/java/com/smartdragons/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/smartdragons/interactivestory/ui/StoryActivity.java
@@ -1,15 +1,28 @@
 package com.smartdragons.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.smartdragons.interactivestory.R;
+import com.smartdragons.interactivestory.model.Page;
+import com.smartdragons.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +30,35 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = getString(R.string.default_name);
+        if (mName == null) {
+            mName = getString(R.string.default_name);
         }
 
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage();
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        // Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+
+        mChoice2.setText(page.getChoice2().getText());
     }
 }
